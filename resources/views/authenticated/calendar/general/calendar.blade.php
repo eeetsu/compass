d9415bdd0cfa9fadc42922af2e231ee1214648f9
eeetsu <gajumaru375@gmail.com>
2024-06-25 20:25:33 +0900

    “修正完了”

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 <div class="vh-100 pt-5" style="background:#ECF1F6">
-  <div class="w-90" style="border-radius:5px background:#FFF">
-    <div class="w-75 m-auto border" style="border-radius:5px">
+  <div class="calendar-iii" style="border-radius:5px background:#FFF">
+    <div class="w-75 m-auto" style="border-radius:5px">
 
       <p class="text-center">{{ $calendar->getTitle() }}</p>
-      <div class="">
+      <div class="calender-m">
         {!! $calendar->render() !!}
       </div>
       <div class="text-right w-75 m-auto">
-        <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts" style="text-align: center;">
+        <div class="calendar-button-two">
+         <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts" style="text-align: center;">
+        </div>
       </div>
 
     </div>
